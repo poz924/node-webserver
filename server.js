e2aa1dda325c637913de924d5d6a2c7e44f8fd7f
Thomas Perraut <poz924@users.noreply.github.com>
2017-10-20 11:37:43 +0200

    correction on project naming

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/project', (req, res) => {
-  res.render('project.hbs', {
-    pageTitle: 'Portfolio',
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
   });
 });
 
